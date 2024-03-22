a725be352e792e021dbc4e883781c87caa803d99
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-22 22:46:14 +0600

    new components added & organized components in dir.

--- withReact/src/App.tsx
+++ withReact/src/App.tsx
@@ -1,6 +1,6 @@
 import DummyText from "./components/DummyText";
 import DragAndDrop from "./components/DragAndDrop";
-import ScrollingProgress from "./components/ScrollingProgress";
+import ScrollingProgress from "./components/Scroll/ScrollingProgress";
 function App() {
   return (
     <>
