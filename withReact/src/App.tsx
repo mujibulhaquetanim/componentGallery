936d398e818e9bb695d466b353080a6bef636d71
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-27 23:58:32 +0600

    inline css added with structure

--- withReact/src/App.tsx
+++ withReact/src/App.tsx
@@ -1,10 +1,12 @@
 import DummyText from "./components/DummyText";
 import DragAndDrop from "./components/DragAndDrop";
 import ScrollingProgress from "./components/Scroll/ScrollingProgress";
+import ProfileCard from "./components/ProfileCard";
 function App() {
   return (
     <>
       <ScrollingProgress />
+      <ProfileCard/>
       <DragAndDrop />
       <DummyText />
     </>
--- withReact/src/components/ProfileCard.tsx
+++ withReact/src/components/ProfileCard.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
 
 export default function ProfileCard() {
   return (
-    <div>ProfileCard</div>
-  )
+    <div className="ufc" style={{border:'2px', margin:'3px'}}>
+      <div className="Gradient" style={{color:"green"}}></div>
+      <div className="Profile-down" style={{color:"green"}}></div>
+      <img className="ufc-img" style={{color:"green"}} src="" alt="Profile Picture" />
+      <div className="ProfileTitle" style={{color:"green"}}></div>
+      <div className="ProfileDescription" style={{color:"green"}}></div>
+    </div>
+  );
 }
