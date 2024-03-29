fcc9e428ec9cbf4e7cd6793b84b79bcfbb33df72
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-29 17:14:56 +0600

    color for selected tags functionality added, type fixes with other bugs.

--- withReact/src/components/MultipleTagSelection/SelectionPage.tsx
+++ withReact/src/components/MultipleTagSelection/SelectionPage.tsx
@@ -8,19 +8,19 @@ interface tag {
 export default function SelectionPage() {
   const [tags, setTags] = useState<tag>({ tag: [] });
 
-  const checkTag = (tag: string) => {
-    tags.tag.filter((item) => item !== tag);
+  const checkTag = (tagName: string) => {
+    return tags.tag.some((item) => item === tagName);
   };
 
-  const selectTag = (tag: string) => {
-    if (tags.tag.some((item) => item === tag)) {
-      const filterTags = tags.tag.filter((item) => item !== tag);
+  const selectTag = (tagName: string) => {
+    if (tags.tag.some((item) => item === tagName)) {
+      const filterTags = tags.tag.filter((item) => item !== tagName);
       setTags((prev) => {
         return { ...prev, tag: filterTags };
       });
     } else {
       setTags((prev) => {
-        return { ...prev, tag: [...prev.tag, tag] };
+        return { ...prev, tag: [...prev.tag, tagName] };
       });
     }
   };
@@ -29,11 +29,15 @@ export default function SelectionPage() {
 
   return (
     <div>
-      <div className="border-2 mt-3 justify-center flex flex">
-        <Tag tagName="JS" selectTag={selectTag} selected={checkTag('JS')} />
-        <Tag tagName="Python" selectTag={selectTag} selected={checkTag('Python')} />
-        <Tag tagName="GO" selectTag={selectTag} selected={checkTag('GO')} />
-        <Tag tagName="C" selectTag={selectTag} selected={checkTag('C')} />
+      <div className="border-2 p-1 mt-3 justify-center flex flex">
+        <Tag tagName="JS" selectTag={selectTag} selected={checkTag("JS")} />
+        <Tag
+          tagName="Python"
+          selectTag={selectTag}
+          selected={checkTag("Python")}
+        />
+        <Tag tagName="GO" selectTag={selectTag} selected={checkTag("GO")} />
+        <Tag tagName="C" selectTag={selectTag} selected={checkTag("C")} />
       </div>
       <div className="gap=3 m-3">You have selected: {tags.tag} </div>
     </div>
--- withReact/src/components/MultipleTagSelection/Tag.tsx
+++ withReact/src/components/MultipleTagSelection/Tag.tsx
@@ -1,19 +1,20 @@
 interface props {
   tagName: string;
-  selected: (tag: string) => void;
+  selected: boolean;
   selectTag: (tagName: string) => void;
 }
 
 export default function tag({ tagName, selectTag, selected }: props) {
-  const tagStyle = {
+  const tagStyle: Record<string, { backgroundColor: string }> = {
     JS: { backgroundColor: "red" },
     Python: { backgroundColor: "green" },
     GO: { backgroundColor: "blue" },
-    C: { backgroundColor: "orange" },
-    default: { backgroundColor: "white" },
+    C: { backgroundColor: "yellow" },
+    default: { backgroundColor: "#AEE0B5" },
   };
   return (
     <button
+      className="border-2 ml-5 p-3 rounded-lg hover:scale-105"
       style={selected ? tagStyle[tagName] : tagStyle.default}
       type="button"
       onClick={() => selectTag(tagName)}
