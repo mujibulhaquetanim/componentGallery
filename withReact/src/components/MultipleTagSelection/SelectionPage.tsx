12e18dc002f8d23ade00c2ebc30f833b6df19459
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-29 17:20:04 +0600

    some method replaced with include method and refactored the code.

--- withReact/src/components/MultipleTagSelection/SelectionPage.tsx
+++ withReact/src/components/MultipleTagSelection/SelectionPage.tsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 import Tag from "./Tag";
 
-interface tag {
+interface TagState {
   tag: string[];
 }
 
 export default function SelectionPage() {
-  const [tags, setTags] = useState<tag>({ tag: [] });
+  const [tags, setTags] = useState<TagState>({ tag: [] });
 
   const checkTag = (tagName: string) => {
-    return tags.tag.some((item) => item === tagName);
+    return tags.tag.includes(tagName);
   };
 
   const selectTag = (tagName: string) => {
-    if (tags.tag.some((item) => item === tagName)) {
-      const filterTags = tags.tag.filter((item) => item !== tagName);
-      setTags((prev) => {
-        return { ...prev, tag: filterTags };
-      });
+    if (tags.tag.includes(tagName)) {
+      const updatedTags = tags.tag.filter((item) => item !== tagName);
+      setTags((prev) => ({ ...prev, tag: updatedTags }));
     } else {
-      setTags((prev) => {
-        return { ...prev, tag: [...prev.tag, tagName] };
-      });
+      setTags((prev) => ({ ...prev, tag: [...prev.tag, tagName] }));
     }
   };
 
@@ -29,17 +25,13 @@ export default function SelectionPage() {
 
   return (
     <div>
-      <div className="border-2 p-1 mt-3 justify-center flex flex">
+      <div className="border-2 p-1 mt-3 justify-center flex">
         <Tag tagName="JS" selectTag={selectTag} selected={checkTag("JS")} />
-        <Tag
-          tagName="Python"
-          selectTag={selectTag}
-          selected={checkTag("Python")}
-        />
+        <Tag tagName="Python" selectTag={selectTag} selected={checkTag("Python")} />
         <Tag tagName="GO" selectTag={selectTag} selected={checkTag("GO")} />
         <Tag tagName="C" selectTag={selectTag} selected={checkTag("C")} />
       </div>
-      <div className="gap=3 m-3">You have selected: {tags.tag} </div>
+      <div className="gap-3 m-3">You have selected: {tags.tag.join(", ")}</div>
     </div>
   );
 }
