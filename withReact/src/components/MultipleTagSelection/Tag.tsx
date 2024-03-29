431475e5b7816deb723dcc7ad8a073d1f2ddae0e
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-29 17:21:59 +0600

    refactored it typescript.

--- withReact/src/components/MultipleTagSelection/Tag.tsx
+++ withReact/src/components/MultipleTagSelection/Tag.tsx
@@ -1,25 +1,34 @@
-interface props {
+import React from "react";
+
+interface TagProps {
   tagName: string;
   selected: boolean;
   selectTag: (tagName: string) => void;
 }
 
-export default function tag({ tagName, selectTag, selected }: props) {
-  const tagStyle: Record<string, { backgroundColor: string }> = {
-    JS: { backgroundColor: "red" },
-    Python: { backgroundColor: "green" },
-    GO: { backgroundColor: "blue" },
-    C: { backgroundColor: "yellow" },
-    default: { backgroundColor: "#AEE0B5" },
+const tagStyle: Record<string, { backgroundColor: string }> = {
+  JS: { backgroundColor: "red" },
+  Python: { backgroundColor: "green" },
+  GO: { backgroundColor: "blue" },
+  C: { backgroundColor: "yellow" },
+  default: { backgroundColor: "#AEE0B5" },
+};
+
+const Tag: React.FC<TagProps> = ({ tagName, selectTag, selected }) => {
+  const handleClick = () => {
+    selectTag(tagName);
   };
+
   return (
     <button
       className="border-2 ml-5 p-3 rounded-lg hover:scale-105"
       style={selected ? tagStyle[tagName] : tagStyle.default}
       type="button"
-      onClick={() => selectTag(tagName)}
+      onClick={handleClick}
     >
       {tagName}
     </button>
   );
-}
+};
+
+export default Tag;
