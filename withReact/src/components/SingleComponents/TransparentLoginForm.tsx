bd8b872f84970e9682b8ac8745852b1ccbda45c6
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-15 23:59:27 +0600

    new component added {login form}

---
+++ withReact/src/components/SingleComponents/TransparentLoginForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TransparentLoginForm() {
+  return (
+    <div>TransparentLoginForm</div>
+  )
+}
+
+export default TransparentLoginForm
+
+// link: https://dev.to/peboy/how-to-create-the-perfect-transparent-login-form-2lfa?ref=dailydev
