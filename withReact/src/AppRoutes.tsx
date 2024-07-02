984eddf00bc6eb2109e36cd07234fdea5f6a31dd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-03 01:27:30 +0600

    react router added for the project

---
+++ withReact/src/AppRoutes.tsx
@@ -0,0 +1,30 @@
+import Components from "./Layouts/Components";
+import ErrorPage from "./Layouts/Error";
+import DragAndDrop from "./components/SingleComponents/DragAndDrop";
+import ScrollingProgress from "./components/Scroll/ScrollingProgress";
+import SelectionPage from "./components/MultipleTagSelection/SelectionPage";
+import ProfileCard from "./components/SingleComponents/ProfileCard";
+import ReactForm from "./components/Form/ReactForm";
+import ReactHookForm from "./components/Form/ReactHookForm";
+import { createBrowserRouter } from "react-router-dom";
+// import ProgressBar from "./components/SingleComponents/ProgressBar";
+
+export const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Components />,
+    children: [
+      { path: "dragAndDrop", element: <DragAndDrop /> },
+      { path: "scrollingProgress", element: <ScrollingProgress /> },
+      { path: "selectionPage", element: <SelectionPage /> },
+      { path: "profileCard", element: <ProfileCard /> },
+      { path: "reactForm", element: <ReactForm /> },
+      { path: "reactHookForm", element: <ReactHookForm /> },
+      // { path: "progressBar", element: <ProgressBar /> },
+    ],
+  },
+  {
+    path: "/*",
+    element: <ErrorPage />,
+  },
+]);
--- withReact/src/main.tsx
+++ withReact/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom'
+import { route } from './AppRoutes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </React.StrictMode>,
 )
