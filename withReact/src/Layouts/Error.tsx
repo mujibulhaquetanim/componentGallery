48341eddd9e0c1967e58d2ecfd0a9c6d77c6ad0a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-04 01:24:52 +0600

    implemented state update for countdown with double setTimeOut inside of useEffect

--- withReact/src/Layouts/Error.tsx
+++ withReact/src/Layouts/Error.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function ErrorPage() {
-  {
-    setTimeout(() => {
-      window.location.href = "/";
-    }, 3000);
-  }
+  const [countdown, setCountdown] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCount) => prevCount - 1);
+    }, 1000);
+
+    const redirect = setTimeout(() => {
+      navigate("/");
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+      clearTimeout(redirect);
+    };
+  }, [navigate]);
+
   return (
-    <div className="h-screen grid place-items-center">
-      <div className="text-3xl font-bold">
-        {" "}
-        Are you lost?
-        <p>Redirecting you to home page in 3 seconds</p>
-      </div>
+    <div>
+      <h1>Error 404: Page Not Found</h1>
+      <p>Redirecting to home page in {countdown} seconds...</p>
     </div>
   );
 }
