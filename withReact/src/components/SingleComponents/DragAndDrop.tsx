b11aa62fba53be63a25dfc0f184a83e5c0d99c27
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-06 23:55:24 +0600

    structure for drag & drop added.

--- withReact/src/components/SingleComponents/DragAndDrop.tsx
+++ withReact/src/components/SingleComponents/DragAndDrop.tsx
@@ -1,9 +1,26 @@
-function DragAndDrop() {
+import React from "react";
+
+export const DragAndDrop: React.FC = function () {
   return (
-    <div className="font-bold text-3xl bg-zinc-700 p-3 w-[10%]">
-      DragAndDrop
+    <div className="">
+      <main className="m-3">
+        <form action="">
+          <input type="color" value={"#ff0000"} />
+          <button type="button">+</button>
+        </form>
+        <div className="list">
+          <div className="note">
+            <span className="close">X</span>
+            <textarea
+              cols={30}
+              rows={10}
+              placeholder="Write content here..."
+            ></textarea>
+          </div>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default DragAndDrop;
