51c3b685e3ac4699b1f7da024f37d8b62ab3aaf1
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-06 23:13:01 +0600

    turned into typescript code and sub navbar added to show dashboard components.

--- withReact/src/components/Private Routes/ProtectedRoutes.tsx
+++ withReact/src/components/Private Routes/ProtectedRoutes.tsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router-dom";
-import LogInReactHookForm from "../Form/LogInReactHookForm";
+import { Link, Outlet } from "react-router-dom";
+import {LogInReactHookForm} from "../Form/LogInReactHookForm";
+import React from "react";
 
-function ProtectedRoutes() {
-  const isSignin = false;
-  return isSignin ? <Outlet /> : <LogInReactHookForm />;
-}
+const SubNavBar: React.FC = () => {
+  return (
+    <div>
+      <div className="flex gap-3 m-2 justify-center">
+        <Link className="border-2 p-2 rounded-md" to="user/profile">Profile</Link>
+        <Link className="border-2 p-2 rounded-md" to="user/orders">Orders</Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const ProtectedRoutes: React.FC = () => {
+  const isSignin: boolean = true;
+  return isSignin ? <SubNavBar /> : <LogInReactHookForm />;
+};
 
 export default ProtectedRoutes;
