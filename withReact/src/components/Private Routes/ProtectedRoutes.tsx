1a70f4db86d29fcd26607f18573cf00d38c1734a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-19 23:20:10 +0600

    dropDown menu created, tw-group used.

--- withReact/src/components/Private Routes/ProtectedRoutes.tsx
+++ withReact/src/components/Private Routes/ProtectedRoutes.tsx
@@ -11,6 +11,17 @@ const SubNavBar: React.FC = () => {
         <Link className="border-2 p-2 rounded-md" to="user/orders">
           Orders
         </Link>
+
+        <div className="group relative border-2 p-2 rounded-md">
+          <p>DropDown</p>
+          <div className="group-hover:block hidden absolute top-10 -right-7 drop-shadow-sm bg-white border-2 rounded-md">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-200 text-slate-500">
+              <p className="cursor-pointer text-black">Profile</p>
+              <p className="cursor-pointer text-black">Settings</p>
+              <p className="cursor-pointer text-black">LogOut</p>
+            </div>
+          </div>
+        </div>
       </div>
       <Outlet />
     </div>
