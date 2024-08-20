89815230dd52cd80115b576517ce6ed9b34a41c4
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-20 23:59:55 +0600

    input field and button added

--- withReact/src/components/SingleComponents/RatingStars.tsx
+++ withReact/src/components/SingleComponents/RatingStars.tsx
@@ -1,23 +1,45 @@
-function RatingStars() {
-    const star= 4.3;
-    const ratingStar = Array.from({length: 5}, (_, index) => {
-        const number = index + 0.5;
-        return(
-            <span key={index}>
-                {star >= index + 1 ? "⭐" : star >= number ? "★" : "☆"}
-            </span>
-        )
+import { useState } from "react";
 
-    });
+function RatingStars() {
+  const [rating, setRating] = useState(0);
+  const ratingStar = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {rating >= index + 1 ? "⭐" : rating >= number ? "★" : "☆"}
+      </span>
+    );
+  });
+    //rating can't be negative and more than 5
+
+  const RatingInput = () => {
+    return (event: ChangeEvent<HTMLInputElement>) => {
+      if (event.target.value > 5) {
+        setRating(5);
+      } else if (event.target.value < 0) {
+        setRating(0);
+      } else {
+        setRating(event.target.value);
+      }
+    };
+  };
+
+  
   return (
-    <div>
-        <div>{ratingStar}</div>
-        <h3>Rating Stars:</h3>
+    <div className="flex flex-col justify-center items-center m-2">
+      <div>{ratingStar}</div>
+      <h3>Rating Stars:</h3>
+      <label htmlFor="rating"></label>
+      <input type="text" value={rating} onChange={RatingInput()} className="border" />
+        <div className="flex gap-2">
+        <div onClick={() => setRating(rating + 1)} className="cursor-pointer border p-2 inline-block">+</div>
+        <div onClick={() => setRating(rating - 1)} className="cursor-pointer border p-2 inline-block">-</div>
+        </div>
     </div>
-  )
+  );
 }
 
-export default RatingStars
+export default RatingStars;
 
 // let stars = [];
 // for (let i = 0; i < 5; i++) {
