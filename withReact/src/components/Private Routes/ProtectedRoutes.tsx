2afb369e3e6072b89256c2156e0d8a73138f7432
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-21 19:44:18 +0600

    dropdown menu added to the the hamIcon, other component optimized for responsive design

--- withReact/src/components/Private Routes/ProtectedRoutes.tsx
+++ withReact/src/components/Private Routes/ProtectedRoutes.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const SubNavBar: React.FC = () => {
   return (
     <div>
-      <div className="flex gap-3 m-1 justify-center shadow-md p-2">
+      <div className="flex gap-3 m-1 justify-evenly shadow-md p-2">
         <Link className="border-2 p-2 rounded-md" to="user/profile">
           Profile
         </Link>
@@ -12,7 +12,7 @@ const SubNavBar: React.FC = () => {
           Orders
         </Link>
 
-        <div className="group relative border-2 p-2 rounded-md">
+        <div className="group relative border-2 p-2 rounded-md hidden sm:block">
           <p>DropDown</p>
           <div className="group-hover:block hidden absolute top-10 -right-7 drop-shadow-sm bg-white border-2 rounded-md">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-200 text-slate-500">
@@ -23,7 +23,7 @@ const SubNavBar: React.FC = () => {
           </div>
         </div>
 
-        <div className="border-2 p-2 rounded-md group relative cursor-pointer">
+        <div className="border-2 p-2 rounded-md group relative cursor-pointer hidden sm:block">
           <p className="rotate-90 text-xl m-auto">⫴</p>
           <div className="cursor-pointer absolute bottom-10 -right-16 rounded-xl border-2 group-hover:block hidden">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-200 text-slate-500">
@@ -34,8 +34,15 @@ const SubNavBar: React.FC = () => {
           </div>
         </div>
 
-        <div className="cursor-pointer sm:hidden">
+        <div className="group relative cursor-pointer sm:hidden ml-3 ">
           <p className="rotate-90 text-5xl">⫴</p>
+          <div className="group-hover:block hidden absolute top-10 -right-7 drop-shadow-sm bg-white border-2 rounded-md">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-200 text-slate-500">
+              <p className="cursor-pointer text-black">Profile</p>
+              <p className="cursor-pointer text-black">Settings</p>
+              <p className="cursor-pointer text-black">LogOut</p>
+            </div>
+          </div>
         </div>
       </div>
       <Outlet />
