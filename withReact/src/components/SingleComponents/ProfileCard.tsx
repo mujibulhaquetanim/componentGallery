5f05872120e82056243e100bdb2d3f1e3906436f
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-21 19:46:23 +0600

    completed the profile card, negative margin & z-index, w-full added to the classNames

--- withReact/src/components/SingleComponents/ProfileCard.tsx
+++ withReact/src/components/SingleComponents/ProfileCard.tsx
@@ -4,12 +4,12 @@ import profilePic from "../../assets/profilePic.png";
 export const ProfileCard: React.FC = function () {
   return (
     <div className="h-screen grid place-items-center">
-      <div className="upc m-3 grid place-items-center border-2 p-3 w-1/3">
-        <div>
-          <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% w-full h-20"></div>
-          <div className="image flex justify-center">
+      <div className="upc m-3 grid place-items-center border w-1/3 rounded-md">
+        <div className="w-full">
+          <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% w-full h-40 drop-shadow-2xl"></div>
+          <div className="image flex justify-center -mt-24">
             <img
-              className="w-36 h-36 rounded-full"
+              className="w-36 h-36 rounded-full z-10"
               src={profilePic}
               alt="Profile Picture"
             />
@@ -20,7 +20,7 @@ export const ProfileCard: React.FC = function () {
           <div className="ProfileTitle justify-center flex text-3xl font-bold">
             Mohammed Hijab
           </div>
-          <div className="ProfileDescription">
+          <div className="text-justify m-2 p-1">
             Mohammed Hijab is a debater and public speaker who engages in
             discussions and polemics on a wide variety of topics including
             religion, politics and society.
