ce10e8141ef3a3df33d7e2110a1994b2e2829b3e
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-01 23:03:01 +0600

    popOver component added [NextUI]

--- third-party-uis/src/NextUI/TooltipComponent.tsx
+++ third-party-uis/src/NextUI/TooltipComponent.tsx
@@ -1,4 +1,5 @@
 import { Tooltip, Button } from "@nextui-org/react";
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover";
 
 export default function TooltipComponent() {
   return (
@@ -8,6 +9,18 @@ export default function TooltipComponent() {
           ToolTip
         </Button>
       </Tooltip>
+
+      <Popover placement="right">
+        <PopoverTrigger>
+          <Button>Open Popover</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="px-1 py-2 text-black">
+            <div className="text-small font-bold">Popover Content</div>
+            <div className="text-tiny">This is the popover content</div>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
