7c97287d619480ef3d34de8a19c65dc8942a5c76
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-03 20:54:49 +0600

    using ref() created another text animation (bounce.inOut)

--- third-party-uis/src/Gsap/TextPluginComponent.tsx
+++ third-party-uis/src/Gsap/TextPluginComponent.tsx
@@ -6,6 +6,8 @@ gsap.registerPlugin(TextPlugin);
 
 function TextPluginComponent() {
   const text = useRef(null);
+  const basicText = useRef(null);
+
   gsap.to(text.current, {
     text: "TextPlugin is here",
     duration: 2,
@@ -13,10 +15,24 @@ function TextPluginComponent() {
     yoyo: true,
     ease: "sine.in",
   });
+
+  gsap.to(basicText.current, {
+    text: "Sample Text",
+    duration: 5,
+    repeat: -1,
+    yoyo: true,
+    ease: "bounce.inOut",
+    delay: 1,
+  });
+
   return (
     <div>
       <h1 className="basicText mt-3" ref={text}>
         TextPlugin
+      </h1>
+
+      <h1 className="basicText mt-3" ref={basicText}>
+        Basic Text
       </h1>
     </div>
   );
