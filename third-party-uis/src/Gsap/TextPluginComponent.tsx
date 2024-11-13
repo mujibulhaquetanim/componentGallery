7a899f07cd2669da0edb0331d2d0b8923ba1d4aa
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-13 23:48:53 +0600

    dependency controlled component added which only render once the dependency triggered

--- third-party-uis/src/Gsap/TextPluginComponent.tsx
+++ third-party-uis/src/Gsap/TextPluginComponent.tsx
@@ -1,13 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 gsap.registerPlugin(TextPlugin);
 
 function TextPluginComponent() {
   const text = useRef(null);
   const basicText = useRef(null);
+  const countDiv = useRef(null);
+  const [count, setCount] = useState(0);
 
   useGSAP(() => {
     gsap.to(text.current, {
@@ -28,10 +30,25 @@ function TextPluginComponent() {
       delay: 1,
       backgroundColor: "green",
     });
-  }, [text, basicText]);
+  });
+
+  useGSAP(
+    () => {
+      gsap.to(countDiv.current, {
+        x: 100,
+        y: 70 + count,
+        ease: "bounce",
+        duration: 3,
+        delay: 2,
+        color: "black",
+      });
+    },
+    { dependencies: [count] }
+  );
 
   return (
     <div>
+      <h1 ref={countDiv}>{count}</h1>
       <h1 className="basicText mt-3" ref={text}>
         TextPlugin
       </h1>
@@ -39,6 +56,8 @@ function TextPluginComponent() {
       <h1 className="basicText mt-3" ref={basicText}>
         Basic Text
       </h1>
+
+      <button onClick={() => setCount((prev) => prev + 50)}>Increment</button>
     </div>
   );
 }
