e8c65bf1b16e59ae931e6bd382a72cd03800240a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-15 00:05:54 +0600

    search filter feature added with bootstrap form ui

--- withReact/src/components/SearchFilter/SearchFilter.tsx
+++ withReact/src/components/SearchFilter/SearchFilter.tsx
@@ -1,36 +1,54 @@
 import { dummyData } from "./dummyData";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 import { Container } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 export default function SearchFilter() {
+  const [search, setSearch] = useState("");
   return (
-    <div className="h-screen grid place-items-center">
-        <h1 className="text-3xl font-bold">Search Filter</h1>
-        <Container>
-        <Table striped bordered hover className="text-center">
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Gender</th>
+    <Container>
+      <h1 className="text-3xl font-bold text-center mt-1">Search Filter</h1>
+      <Form>
+        <Form.Group className="m-4">
+          <Form.Control
+            size="lg"
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search"
+          />
+        </Form.Group>
+      </Form>
+
+      <Table striped bordered hover className="text-center">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dummyData
+            .filter((item) =>
+              item.first_name === ""
+                ? item
+                : item.first_name.toLowerCase().includes(search)
+            )
+            .map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.gender}</td>
                 </tr>
-            </thead>
-            <tbody>
-                {dummyData.map((item)=>{
-                    return(
-                        <tr key={item.id}>
-                            <td>{item.first_name}</td>
-                            <td>{item.last_name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.gender}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </Table>
-        </Container>
-    </div>
-  )
+              );
+            })}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
