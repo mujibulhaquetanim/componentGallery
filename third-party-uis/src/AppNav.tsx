adad62910a1f6d760716d28d7cc501ffad806a95
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-12-31 22:36:34 +0600

    App.tsx -> AppNav. now will be used for homePage navBar and as a parent component for react-router v7

---
+++ third-party-uis/src/AppNav.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router";
+const AppNav: React.FC = function () {
+  return (
+    <div className="h-screen">
+      <nav className="flex gap-3 justify-evenly shadow-md p-2 border bg-slate-300 w-full mb-4">
+        <NavLink
+          className="border-2 p-2 rounded-md  hover:bg-slate-400"
+          to="/"
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className="border-2 p-2 rounded-md hover:bg-slate-400"
+          to="/gsap"
+          end
+        >
+          Gsap
+        </NavLink>
+        <NavLink
+          className="border-2 p-2 rounded-md hover:bg-slate-400"
+          to="/swiper"
+          end
+        >
+          Swiper
+        </NavLink>
+        <NavLink
+          className="border-2 p-2 rounded-md hover:bg-slate-400"
+          to="/aos"
+          end
+        >
+          AOS
+        </NavLink>
+        <NavLink
+          className="border-2 p-2 rounded-md hover:bg-slate-400"
+          to="/framer-motion"
+          end
+        >
+          Framer Motion
+        </NavLink>
+        <NavLink
+          className="border-2 p-2 rounded-md hover:bg-slate-400"
+          to="/toastify"
+          end
+        >
+          Toastify
+        </NavLink>
+        <NavLink
+          className="border-2 p-2 rounded-md hover:bg-slate-400"
+          to="/next-ui"
+          end
+        >
+          Next UI
+        </NavLink>
+      </nav>
+      <h1 className="text-2xl font-bold mt-4">Welcome to Third-Party UIs</h1>
+      <p className="mb-4">
+        Click on the navigation items to view different components.
+      </p>
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppNav;
