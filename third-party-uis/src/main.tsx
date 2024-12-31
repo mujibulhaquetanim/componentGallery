71e46ae2dd5e638b1285a624a82e96416f368569
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-12-31 22:37:34 +0600

    routes configured using React-Router v7.1.1

--- third-party-uis/src/main.tsx
+++ third-party-uis/src/main.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import AppNav from "./AppNav.tsx";
 import { NextUIProvider } from "@nextui-org/react";
-import "./index.css";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Swiper from "./swiper/Swiper.tsx";
+import GsapIndex from "./Gsap/GsapIndex.tsx";
+import FramerMotionIndex from "./framer-motion/FramerMotionIndex.tsx";
+import ToastifyIndex from "./Toastify/ToastifyIndex.tsx";
+import AosIndex from "./aos/AosIndex.tsx";
+import NextUiIndex from "./NextUI/NextUiIndex.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
-    </BrowserRouter>
+    <NextUIProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppNav />}>
+            <Route path="swiper" element={<Swiper />} />
+            <Route path="gsap" element={<GsapIndex />} />
+            <Route path="framer-motion" element={<FramerMotionIndex />} />
+            <Route path="toastify" element={<ToastifyIndex />} />
+            <Route path="aos" element={<AosIndex />} />
+            <Route path="next-ui" element={<NextUiIndex />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </NextUIProvider>
   </React.StrictMode>
 );
