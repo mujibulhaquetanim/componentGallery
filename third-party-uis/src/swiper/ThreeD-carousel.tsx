66b968d6ba95452727785ca2de2dc39f5fab8c88
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-12-31 10:49:47 +0600

    refactor 3D carousel component to use dynamic image mapping and styles.css->tailwindcss

--- third-party-uis/src/swiper/ThreeD-carousel.tsx
+++ third-party-uis/src/swiper/ThreeD-carousel.tsx
@@ -1,69 +1,51 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-
-import "./styles.css";
-
-// import required modules
-import { EffectCoverflow} from "swiper/modules";
+import { EffectCoverflow } from "swiper/modules";
 
 export default function CarouselThreeD() {
+  const images = [
+    "https://swiperjs.com/demos/images/nature-1.jpg",
+    "https://swiperjs.com/demos/images/nature-2.jpg",
+    "https://swiperjs.com/demos/images/nature-3.jpg",
+    "https://swiperjs.com/demos/images/nature-4.jpg",
+    "https://swiperjs.com/demos/images/nature-5.jpg",
+    "https://swiperjs.com/demos/images/nature-6.jpg",
+    "https://swiperjs.com/demos/images/nature-7.jpg",
+    "https://swiperjs.com/demos/images/nature-8.jpg",
+    "https://swiperjs.com/demos/images/nature-9.jpg",
+  ];
+
   return (
-    <>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        spaceBetween={0}
-        loop={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 50,
-          modifier: 1.5,
-          slideShadows: true,
-        }}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={false}
-        modules={[EffectCoverflow]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
+    <Swiper
+      effect={"coverflow"}
+      grabCursor={true}
+      centeredSlides={true}
+      spaceBetween={0}
+      loop={true}
+      slidesPerView={"auto"}
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 0,
+        depth: 4,
+        modifier: 12,
+        slideShadows: true,
+      }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[EffectCoverflow]}
+      className="swiper w-full pt-12 pb-12"
+    >
+      {images.map((src, index) => (
+        <SwiperSlide
+          key={index}
+          className="swiper-slide flex bg-center bg-cover w-[200px] h-[200px]"
+        >
+          <img className="block w-full" src={src} alt={`Slide ${index + 1}`} />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="" />
-        </SwiperSlide>
-      </Swiper>
-    </>
+      ))}
+    </Swiper>
   );
 }
