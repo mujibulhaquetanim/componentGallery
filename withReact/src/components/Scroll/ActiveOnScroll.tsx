2c88479e6d2aef4f076d411b44b3c29667a3df83
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-04 21:58:37 +0600

    removed imported react library as it was not used directly in these files

--- withReact/src/components/Scroll/ActiveOnScroll.tsx
+++ withReact/src/components/Scroll/ActiveOnScroll.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ActiveOnScroll() {
   return (
     <div>ActiveOnScroll</div>
--- withReact/src/components/SingleComponents/ContactFormDataOnEmail.tsx
+++ withReact/src/components/SingleComponents/ContactFormDataOnEmail.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ContactFormDataOnEmail() {
   return (
     <div>ContactFormDataOnEmail</div>
