cc9c2e2026866cecbbe9cd5a5c5f8a566436f7c9
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-05 12:46:16 +0600

    transition added + checking with oldID to newId added in the setState. improved css

--- withReact/src/components/Accordion/DynamicAccordion.tsx
+++ withReact/src/components/Accordion/DynamicAccordion.tsx
@@ -15,7 +15,7 @@ export default function DynamicAccordion({
   const show = activeId === id;
   return (
     <div
-      className={`border-2 rounded-xl p-3 w-1/3 space-y-4 border-[#bc85de] ${
+      className={`border-2 rounded-xl px-3 py-2 space-y-2 w-1/3 border-[#bc85de] ${
         show && "bg-[#f1ecfb]"
       }`}
     >
@@ -24,18 +24,26 @@ export default function DynamicAccordion({
         <p className="w-4/5 font-bold">{question}</p>
         <button
           className="absolute right-0"
-          onClick={() => setActiveId(activeId ? null : id)}
+          onClick={() => setActiveId(activeId === id ? null : id)}
         >
           {show ? (
             <div className="px-2 text-xl border rounded-lg bg-[#a254d0] text-white transition duration-1000 ease-in">
               -
             </div>
           ) : (
-            <div className="text-xl transition duration-300 ease-out">+</div>
+            <div className="text-xl transition duration-300 ease-out px-1 rounded-xl">
+              +
+            </div>
           )}
         </button>
       </div>
-      {show ? <p className={`w-4/5 mx-auto text-justify`}>{answer}</p> : ""}
+      <div
+        className={`overflow-hidden transition-all duration-1000 ease-in-out ${
+          show ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <p className="w-4/5 mx-auto text-justify">{answer}</p>
+      </div>
     </div>
   );
 }
