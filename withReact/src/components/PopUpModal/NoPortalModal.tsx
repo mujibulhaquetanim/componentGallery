e17a0915077e1a1727e1eadd20da1196b985d8c0
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-18 23:22:18 +0600

    refactor: enhance NoPortalModal styling and improve button functionality

--- withReact/src/components/PopUpModal/NoPortalModal.tsx
+++ withReact/src/components/PopUpModal/NoPortalModal.tsx
@@ -10,13 +10,13 @@ const NoPortalModal: React.FC<Props> = ({ setIsOpen }) => {
       className="flex flex-col top-0 left-0 right-0 bottom-0 fixed bg-[rgba(0,0,0,0.5)]"
       onClick={() => setIsOpen(false)}
     >
-      <div className="flex flex-col gap-4 border-2 p-5 top-[50%] left-[50%] fixed translate-x-[-50%] translate-y-[-50%] bg-slate-100">
+      <div className="flex flex-col gap-4 border-2 p-5 top-[50%] left-[50%] fixed translate-x-[-50%] translate-y-[-50%] bg-slate-100 rounded-md drop-shadow-md">
         <h1 className="text-2xl font-bold flex justify-center">Modal Sample</h1>
         <p>To see the modal, click the button below</p>
         <p>
           To close the modal, click the button in the modal or in the background
         </p>
-        <button className="p-2 border-2 bg-red-500">Close Modal</button>
+        <button onClick={() => setIsOpen(false)} className="flex justify-center gap-3 text-white font-bold p-2 border-2 bg-red-500"><span className="font-bold bg-white">❌</span> Close Modal</button>
       </div>
     </div>
   );
